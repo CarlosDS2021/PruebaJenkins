ebeb4cbabcb0601d58f176a706fd7ce0d1c0111b
CarlosDS2021 <durandsilvacarlos@gmail.com>
2021-04-04 13:24:30 -0500

    first commit

---
+++ src/test/java/com/org/PruebaJenkins/Prueba04042021.java
@@ -0,0 +1,13 @@
+package com.org.PruebaJenkins;
+
+import org.junit.Test;
+
+public class Prueba04042021 {
+	
+	@Test
+	public void imprimeAlgo() {
+		System.out.println("Esto es una prueba en Maven 04042021");
+	}
+
+
+}
